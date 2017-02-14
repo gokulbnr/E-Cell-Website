1ad03ba862c41ab5ea4e527ed8556a7a6161acec
Arjun Nemani <nemaniarjun@gmail.com>
2017-02-14 05:48:54 +0530

    Configured Templates Dir, Made code in Backend PEP8 compilant.

--- Backend_Repo/website/views.py
+++ Backend_Repo/website/views.py
@@ -1,54 +1,70 @@
 from django.shortcuts import render
 
 # home part
+
+
 def home(request):
-	return render(request,'website/home.html')
+    return render(request, 'website/home.html')
 # about part
+
+
 def vision(request):
-	return render(request,'website/vision.html')
+    return render(request, 'website/vision.html')
+
 
 def successStories(request):
-	return render(request,'website/success_stories.html')
+    return render(request, 'website/success_stories.html')
 
 # Events part
 
+
 def talks(request):
-	return render(request,'website/talks.html')
+    return render(request, 'website/talks.html')
+
 
 def hackathons(request):
-	return render(request,'website/hackathons.html')
+    return render(request, 'website/hackathons.html')
+
 
 def workshops(request):
-	return render(request,'website/workshops.html')
+    return render(request, 'website/workshops.html')
 
 # associates part
 
+
 def associates(request):
-	return render(request,'website/associates.html')
+    return render(request, 'website/associates.html')
+
 
 def patners(request):
-	return render(request,'website/patners.html')
+    return render(request, 'website/patners.html')
+
 
 def sponsors(request):
-	return render(request,'website/sponsors.html')
+    return render(request, 'website/sponsors.html')
 
 # gallery
 
+
 def gallery(request):
-	return render(request,'website/gallery.html')
+    return render(request, 'website/gallery.html')
 
 # mentors
 
+
 def industry(request):
-	return render(request,'website/industry.html')
+    return render(request, 'website/industry.html')
+
 
 def students(request):
-	return render(request,'website/students.html')
+    return render(request, 'website/students.html')
+
 
 def faculty(request):
-	return render(request,'website/faculty.html')
+    return render(request, 'website/faculty.html')
 
-# e resources 
+# e resources
+
 
 def eResources(request):
 	return render(request,'website/e-resources.html')
